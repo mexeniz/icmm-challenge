5cf896f96e57ab53f69411eb3dfcfc72a8505856
Pawissakan Chirupphapa <pawissakan.cpcu@gmail.com>
2018-10-23 12:10:15 +0700

    Added time-zone for report script

--- report.py
+++ report.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import csv
+import pytz
 import datetime
 import argparse
 from stravalib.client import Client
@@ -18,11 +19,6 @@ DATABASE_NAME = os.environ["DATABASE_NAME"]
 DEFAULT_OUTPUT_DIR = "./"
 
 STRING_TIME_FORMAT = "%Y-%b-%d %H:%M:%S"
-
-now = datetime.datetime.now()
-timestamp = now.strftime(STRING_TIME_FORMAT)
-report_prefix = now.strftime("%Y%m%d_%H%M%S")
-print("Report timestamp:", timestamp)
 
 ChallengeDB.init(MONGODB_URI, DATABASE_NAME)
 
@@ -46,7 +42,7 @@ def upload_reports(drive_cleint_config, token_path, folder_id, report_paths):
             file_drive.Upload()
             print("Upload file: %s" % (title))
 
-def gen_run_report(report_path):
+def gen_run_report(timestamp, report_path):
     runs = ChallengeDB.find_run()
     with open(report_path, "w", newline="") as csvfile:
         fieldnames = ["timestamp"] + list(runs[0].to_doc().keys())
@@ -63,7 +59,7 @@ def gen_run_report(report_path):
     print("Total Runs:", len(runs))
     print("Generated report to", report_path)
 
-def gen_runner_report(report_path):
+def gen_runner_report(timestamp, report_path):
     runners = ChallengeDB.find_runner()
     with open(report_path, "w", newline="") as csvfile:
         fieldnames = ["timestamp","_id", "displayname", "intania", "createdAt"]
@@ -80,15 +76,25 @@ def gen_runner_report(report_path):
     print("Generated report to", report_path)
 
 def main():
+    if args.time_zone:
+        tz = pytz.timezone(args.time_zone)
+        now = datetime.datetime.now(tz)
+    else:
+        now = datetime.datetime.now()
+
+    timestamp = now.strftime(STRING_TIME_FORMAT)
+    report_prefix = now.strftime("%Y%m%d_%H%M%S")
+    print("Report timestamp:", timestamp)
+
     output_dir = args.output_dir
     os.makedirs(output_dir, exist_ok=True)
     runner_report_name = "%s_runner_report.csv" % (report_prefix)
     run_report_name = "%s_run_report.csv" % (report_prefix)
 
     runner_report_path = os.path.join(output_dir, runner_report_name)
-    gen_runner_report(runner_report_path)
+    gen_runner_report(timestamp, runner_report_path)
     run_report_path = os.path.join(output_dir, run_report_name)
-    gen_run_report(run_report_path)
+    gen_run_report(timestamp, run_report_path)
 
     if args.drive_cleint_config and args.drive_token and args.drive_folder_id:
         print("GDrive config is set, uploading reports to Gdrive.")
@@ -98,11 +104,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--output-dir", help="Output directory for reports", action="store",
                         default=DEFAULT_OUTPUT_DIR, dest="output_dir", type=str)
+    parser.add_argument("--time-zone", help="Timezone for timestamp", action="store",
+                        default=None, dest="time_zone", type=str)
     parser.add_argument("--drive-client-config", help="GDrive client config file", action="store",
-                        default=DEFAULT_OUTPUT_DIR, dest="drive_cleint_config", type=str)
+                        dest="drive_cleint_config", type=str)
     parser.add_argument("--drive-token", help="GDrive access token file", action="store",
-                        default=DEFAULT_OUTPUT_DIR, dest="drive_token", type=str)
+                        dest="drive_token", type=str)
     parser.add_argument("--drive-folder-id", help="Destination folder id on GDrive", action="store",
-                        default=DEFAULT_OUTPUT_DIR, dest="drive_folder_id", type=str)
+                        dest="drive_folder_id", type=str)
     args = parser.parse_args()
     main()
